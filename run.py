fb3e99089de8b1abbd6bdba1c6c8eb3c86cb942a
VIPABH <a891983.2@gmail.com>
2026-09-23 15:20:59 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ async def run_button():
     if not button_token:
         print("❌ خطأ: لم يتم العثور على توكن BUTTON_BOT في المتغيرات البيئية!")
         return
-    await BUTTON_BOT.start(bot_token=button_token)
+    await ABH.start(bot_token=button_token)
     print("✅ BUTTON_BOT يعمل الآن بنجاح!")
     try:
-        await BUTTON_BOT.run_until_disconnected()
+        await ABH.run_until_disconnected()
     except (KeyboardInterrupt, SystemExit):
         pass
     finally:
         print("\n🛑 جاري إغلاق بوت الأزرار...")
-        if BUTTON_BOT.is_connected():
-            await BUTTON_BOT.disconnect()
+        if ABH.is_connected():
+            await ABH.disconnect()
