1fdb21b430e8dbf9c9f5993aceb3564fe25e60c9
VIPABH <a891983.2@gmail.com>
2026-09-23 15:11:37 +0300

    Create client.py

---
+++ client.py
@@ -0,0 +1,17 @@
+from telethon import events, TelegramClient, connection
+import os, redis
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+BUTTON_BOT = TelegramClient(
+    "BUTTON_BOT",
+    api_id,
+    api_hash,
+    connection=connection.ConnectionTcpFull,
+    sequential_updates=False,
+    auto_reconnect=True,
+    connection_retries=None)
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    db=0,
+    decode_responses=True)
