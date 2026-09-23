acfe706a105cf41da5540d00e389bd10204080ff
VIPABH <a891983.2@gmail.com>
2026-09-23 15:16:49 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-import os
-import sys
-import asyncio
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from client import *
-from .button_bot import *
 async def run_button():
     button_token = os.getenv("BUTTON_BOT")
     if not button_token:
