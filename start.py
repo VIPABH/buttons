d7fef992abba715b54e3cba3d23a18d2df6383e0
VIPABH <a891983.2@gmail.com>
2026-09-25 15:47:25 +0300

    Update start.py

--- start.py
+++ start.py
@@ -16,13 +16,13 @@ def buttons(e):
     text = session.get('text')
     media = session.get('media')
     button = session.get('buttons') or []
-    message_button = []
+    message_button = [
+        Button.inline('تعديل نص' if text else 'إضافة نص', data='set_text', icon=5280993797482750213, style=green if text else blue),
+        Button.inline('تعديل ميديا' if media else 'إضافة ميديا', data='set_media', icon=5280993797482750213, style=green if media else blue),
+    ]
     if len(button) <= 1:
         message_button.append(
         Button.inline('تعديل زر' if button else 'إضافة زر', data='set_buttons', icon=5280993797482750213, style=green if button else blue))
-    message_button.append(
-        Button.inline('تعديل نص' if text else 'إضافة نص', data='set_text', icon=5280993797482750213, style=green if text else blue),
-        Button.inline('تعديل ميديا' if media else 'إضافة ميديا', data='set_media', icon=5280993797482750213, style=green if media else blue),)
     del_button = [
         Button.inline('حذف الكل', data='del_all', icon=5465665476971471368, style=red),
         Button.inline('حذف معين', data='delete', icon=5229113891081956317, style=red),
