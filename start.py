20666989f0e1ce81b770e14fc7f03b902f2f6eaf
VIPABH <a891983.2@gmail.com>
2026-09-25 11:45:27 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,32 +1,99 @@
+from telethon import events
 from helpers import *
 from client import *
 import re
 @ABH.on(events.NewMessage)
 async def is_user_check(e):
     await is_user(e)
+    await small_filter(e)
 @ABH.on(events.NewMessage(pattern=r'^/start'))
 async def start(e):
     await send(e, 'اهلا عزيزي ( {await ment(e)} ) اني بوت مال ازرار استخدامي سهل و بسيط \n ارسل `الاوامر`')
 message = {}
+arg = {'text': 'ارسل الان النص', 'media': 'ارسل الان الميديا', 'buttons': 'ارسل الان الزر بالتنسيق الاتي \n اما اسم الزر بعده : وبعده الرابط \nمثال `ابن هاشم:https://t.me/wfffp` \n او اسم الزر بعده الرابط مفصول'}
+b = [
+    [Button.inline('تعيين نص' if not text else 'اضف او تعديل النص' , data='set_text', icon=5280993797482750213, style=green if not text else blue),
+    Button.inline('تعيين ميديا' if not text else 'اضف او تعديل الميديا' , data='set_media', icon=5280993797482750213, style= green if not text else blue),
+    Button.inline('تعيين زر' if not text else 'اضف او تعديل الزر' , data='set_buttons', icon=5280993797482750213, style= green if not text else blue),],
+    [
+    Button.inline('حذف الكل', data='del_all', icon=5465665476971471368, style=red),
+    Button.inline('حذف معين', data='delete', icon=5229113891081956317, style=red),
+    ],
+    [
+    Button.inline('تم', data='done', icon=5854724316385512963, style=green),
+    ]
+]
 @ABH.on(events.NewMessage(pattern=r'^انشاء رسالة$'))
 async def create_message(e):
+    id = e.sender_id
+    if id not in message:
+        message[id] = {'text': '', 'media': [], 'buttons': []}
     session = message.get(e.sender_id)
     text = session.get('text')
     media = session.get('media')
     buttons = session.get('buttons')
-    b = [
-        [Button.inline('تعيين نص' if not text else 'اضف او تعديل النص' , data='set_text', icon=5280993797482750213, style=green if not text else blue),
-        Button.inline('تعيين ميديا' if not text else 'اضف او تعديل الميديا' , data='set_media', icon=5280993797482750213, style= green if not text else blue),
-        Button.inline('تعيين زر' if not text else 'اضف او تعديل الزر' , data='set_url', icon=5280993797482750213, style= green if not text else blue),],
-        [
-        Button.inline('حذف الكل', data='del_all', icon=5465665476971471368, style=red),
-        Button.inline('حذف معين', data='delete', icon=5229113891081956317, style=red),
-        ],
-        [
-        Button.inline('تم', data='done', icon=5854724316385512963, style=green),
-        ]
-    ]
-    await send(e, 'اهلا عزيزي وين تحب نبدي', buttons=b)    
+    await send(e, 'اهلا عزيزي وين تحب نبدي', buttons=b)
+@ABH.on(events.CallbackQuery(data='(set_|del)'))
+async def create_message_claaback(e):
+    data = e.data.decode('utf-8')
+    if e.sender_id not in message:
+        return await e.edit('جلسة انشاء الرساله حذفت , اعد المحاولة')
+    if data == 'del_all':
+        del message[e.sender_id]
+        return await e.edit('تم حذف الجلسة')
+    elif data.startswith('set_'):
+        data = data.replace('set_', '')
+        message[e.sender_id]['step'] = data
+        await e.edit(arg[data])
+async def _send(e):
+    id = e.sender_id
+    if not id in message:return
+    b = [Button.inline(button[0], button[1]) for button in message[e.sender_id]['buttons']]
+    if message[e.sender_id]['media']:
+        media_list = [await get_input_media(media) for media in message[e.sender_id]['media']]
+        await ABH.send_file(e.chat_id, file=media_list, caption=message[e.sender_id]['text'], buttons=b)
+    else:
+        await ABH.send_message(e.chat_id, text=message[e.sender_id]['text'], buttons=b)
+processed_groups = set()
+async def small_filter(e):
+    step = message.get(e.sender_id, None).get('step', None)
+    if not step:return
+    text = e.text
+    if step == 'text':
+        message[e.sender_id]['text'] += text
+        await e.reply('تم اضافة النص')
+        await _send(e)
+        del message[e.sender_id]['step']
+    elif step == 'media':
+        if e.media:
+            # gid = getattr(msg, 'grouped_id', None)
+            # if msg.media and gid:
+            #     if gid in processed_groups:
+            #         return
+            #     processed_groups.add(gid)
+            message[e.sender_id]['media'].append(await extract_media_data(e))
+            await e.reply('تم اضافة الميديا')
+            await _send(e)
+            del message[e.sender_id]['step']
+        else:
+            await e.reply('عذرا عزيزي لازم ترسل ميديا مناسبة')
+            await _send(e)
+            del message[e.sender_id]['step']
+    elif step == 'buttons':
+        if ':' in text:
+            message[e.sender_id]['buttons'].append(text.split(':'))
+            await e.reply('تم اضافة الزر')
+            await _send(e)
+            del message[e.sender_id]['step']
+        else:
+            message[e.sender_id]['temp_btn_name'] = text
+            message[e.sender_id]['step'] = 'button_name'
+            await e.reply('تم اضافة اسم الزر\n ارسل الرابط')
+    elif step == 'button_name':
+        button_name = message[e.sender_id]['temp_btn_name']
+        del message[e.sender_id]['temp_btn_name']
+        message[e.sender_id]['buttons'].append((button_name, text))
+        await e.reply('تم اضافة الزر')
 @ABH.on(events.NewMessage(pattern=r'^الاوامر'))
 async def command(e):
     await e.reply(
@@ -158,4 +225,3 @@ async def handler(event):
             await ABH.send_message(event.chat_id, f"تم إنشاء الأزرار بنجاح.{warning}")
     except Exception:
         return await event.reply("حدث خطأ أثناء إنشاء الرسالة والأزرار.")
-print('ABH شغال')
