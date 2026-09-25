c82d710a00e640854ae7533625d87f2faa03f9fa
VIPABH <a891983.2@gmail.com>
2026-09-25 12:58:20 +0300

    Update start.py

--- start.py
+++ start.py
@@ -18,9 +18,9 @@ async def create_message(e):
         message[id] = {'text': '', 'media': [], 'buttons': []}
     session = message.get(e.sender_id)
     text = session.get('text')
-    b = [[Button.inline('تعيين نص' if not text else 'اضف او تعديل النص' , data='set_text', icon=5280993797482750213, style=blue if not text else green),
-        Button.inline('تعيين ميديا' if not text else 'اضف او تعديل الميديا' , data='set_media', icon=5280993797482750213, style= blue if not text else green),
-        Button.inline('تعيين زر' if not text else 'اضف او تعديل الزر' , data='set_buttons', icon=5280993797482750213, style= blue if not text else green),],
+    b = [[Button.inline('تعيين نص' if not text else 'اضف نص' , data='set_text', icon=5280993797482750213, style=blue if not text else green),
+        Button.inline('تعيين ميديا' if not text else 'اضف ميديا' , data='set_media', icon=5280993797482750213, style= blue if not text else green),
+        Button.inline('تعيين زر' if not text else 'اضف زر' , data='set_buttons', icon=5280993797482750213, style= blue if not text else green),],
         [Button.inline('حذف الكل', data='del_all', icon=5465665476971471368, style=red),
         Button.inline('حذف معين', data='delete', icon=5229113891081956317, style=red),],
         [Button.inline('تم', data='done', icon=5854724316385512963, style=green),]]
@@ -39,23 +39,60 @@ async def create_message_claaback(e):
         message[e.sender_id]['step'] = data
         await e.edit(arg[data])
 async def _send(e):
-    id = e.sender_id
-    if not id in message:return
-    b = [Button.inline(button[0], button[1]) for button in message[e.sender_id]['buttons']]
-    if message[e.sender_id]['media']:
-        if len(message[e.sender_id]['media']) > 1:
-            media = [await get_input_media(m) for m in message[e.sender_id]['media']]
+    user_id = e.sender_id    
+    if user_id not in message:
+        return
+    session = message[user_id]
+    text = session.get('text') or "معاينة الرسالة:"
+    raw_media = session.get('media') or []
+    raw_buttons = session.get('buttons') or []
+    formatted_buttons = []
+    for btn in raw_buttons:
+        if not btn:
+            continue
+        if isinstance(btn, (list, tuple)) and len(btn) >= 2:
+            name, target = str(btn[0]), str(btn[1])
+            if target.startswith(('http://', 'https://', 't.me', 'tg://')):
+                formatted_buttons.append([Button.url(name, target)])
+            else:
+                formatted_buttons.append([Button.inline(name, data=target.encode('utf-8'))])
+        elif isinstance(btn, Button):
+            formatted_buttons.append([btn] if not isinstance(btn, list) else btn)
+    buttons_to_send = formatted_buttons if formatted_buttons else None
+    if raw_media:
+        processed_media = []
+        for m in raw_media:
+            item = await get_input_media(m) if callable(get_input_media) else m
+            if item is not None:
+                processed_media.append(item)
+        if not processed_media:
+            await ABH.send_message(e.chat_id, message=text, buttons=buttons_to_send)
+            return
+        if len(processed_media) == 1:
+            await ABH.send_file(
+                e.chat_id,
+                file=processed_media[0],
+                caption=text,
+                buttons=buttons_to_send
+            )
         else:
-            media = await get_input_media(message[e.sender_id]['media'])
-        await ABH.send_file(e.chat_id, file=media, caption=message[e.sender_id]['text'], buttons=b)
+            await ABH.send_file(
+                e.chat_id,
+                file=processed_media,
+                caption=text,
+                buttons=None
+            )
+            if buttons_to_send:
+                await ABH.send_message(e.chat_id, message="🔘 الأزرار المرفقة:", buttons=buttons_to_send)
     else:
-        await ABH.send_message(e.chat_id, message=message[e.sender_id]['text'], buttons=b)
+        await ABH.send_message(e.chat_id, message=text, buttons=buttons_to_send)
 processed_groups = set()
 async def small_filter(e):
     session = message.get(e.sender_id, None)
     if not session:return
     step = session.get('step')
     text = e.text
+    if text == 'انشاء رسالة':return
     if step == 'text':
         message[e.sender_id]['text'] += text
         await e.reply('تم اضافة النص')
